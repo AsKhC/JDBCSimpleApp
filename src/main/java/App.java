03ddf872938c235d153d8607f9482c8d0dca36a6
AsKhC <askh.person@gmail.com>
2022-04-08 05:46:17 +0300

    Added to file 'DBOperations' a method for adding new students to DB. Wrote an algorith to choose any operation to work with DB in a file 'App'.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,39 +4,34 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 import ConnectionDao.ConnectionDao;
+import ConnectionDao.DBOperations;
 
 public class App {
+	
 	final static Scanner scanner = new Scanner(System.in);
-
+	final static ConnectionDao connectionDao = new ConnectionDao();
+	final static DBOperations dbOperations = new DBOperations();
+	
 	public static void main(String args[]) throws ClassNotFoundException, SQLException {
-		final ConnectionDao connectionDao = new ConnectionDao();
+		
 		final Connection connection = connectionDao.getConnection();
-		int numberOfOperation = 0;
+		int numberOfOperation;
+		
 		
 		do {
 			numberOfOperation = chooseOperation();
-				
+			
 			if (numberOfOperation == 1) {
 				System.out.println("Number is 1");
+				dbOperations.operationExecuteUpdate();
 			} else if (numberOfOperation == 2) {
 				System.out.println("Number is 2");
-			} else if (numberOfOperation != 1 || numberOfOperation != 2){
-				System.out.println("Error on choosing operation number");
-				chooseOperation();
+			} else {
+				System.out.println("Error on choosing operation number!");
 			}
-		} while (numberOfOperation != 1 || numberOfOperation != 2);
+		} while (numberOfOperation != 1 & numberOfOperation != 2);
 		
-		
-//		String id = scanner.next();
-//		String email = scanner.next();
-//		String first_name = scanner.next();
-//		String last_name = scanner.next();
-//		
-//		Statement statement = connection.createStatement();
-//		
-//		statement.executeUpdate("INSERT INTO `mydb`.`student` (`id`, `email`, `first_name`, `last_name`)  VALUES ('" + id + "', '" + email + "', '" + first_name + "', '" + last_name + "');");
-//		
-//		statement.close();
+
 		
 		
 		
@@ -46,9 +41,10 @@ public class App {
 		System.out.println("Hello! What do we do with DB? \n"
 				+ "Choose operation:\n"
 				+ "1. Add new data\n"
-				+ "2. Show data\n");
-		
-		return scanner.nextInt();
+				+ "2. Show data by id\n");
+		int back = scanner.nextInt();
+		return back;
 	}
 	
+
 }
--- src/main/java/ConnectionDao/DBOperations.java
+++ src/main/java/ConnectionDao/DBOperations.java
@@ -3,19 +3,37 @@ package ConnectionDao;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class DBOperations {
 	
-	final ConnectionDao connectionDao = new ConnectionDao();
+	final static ConnectionDao connectionDao = new ConnectionDao();
+	final static Scanner scanner = new Scanner(System.in);
 	
-	public void databaseExecuteQuery(String id, String email, String first_name, String last_name) throws ClassNotFoundException, SQLException {
+	public static void operationExecuteUpdate() throws ClassNotFoundException, SQLException{
 		
 		final Connection connection = connectionDao.getConnection();
 
+		//Entering data to add new student to DB
+		System.out.println("Enter student's ID");
+		String id = scanner.next();
+		System.out.println("Enter student's EMAIL");
+		String email = scanner.next();
+		System.out.println("Enter student's FIRST_NAME");
+		String first_name = scanner.next();
+		System.out.println("Enter student's LAST_NAME");
+		String last_name = scanner.next();
+		
+		//Creating a Statement to execute SQL query to add new student
 		Statement statement = connection.createStatement();
-		statement.executeUpdate("INSERT INTO `mydb`.`student` (`id`, `email`, `first_name`, `last_name`)  VALUES "
-				+ "('" + id + "', '" + email + "', '" + first_name + "', '" + last_name + "');");
+		statement.executeUpdate("INSERT INTO `mydb`.`student` (`id`, `email`, `first_name`, `last_name`)  "
+				+ "VALUES ('" + id + "', '" + email + "', '" + first_name + "', '" + last_name + "');");
+		
+		//Closing the Statement to free used recources
 		statement.close();
+		
+		System.out.println("Successfully added new student.");
+		
 	}
 	
 	public void databaseExecuteQuery() throws ClassNotFoundException, SQLException{
